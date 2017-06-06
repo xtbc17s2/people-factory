d6917ce07a8646f60cbe13a823402206eec1ac5d
Dave Strus <dave@getfretless.com>
2017-06-06 10:13:14 -0400

    Show all form values in details

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ function handleSubmit(ev) {
   const f = ev.target
   const details = document.querySelector('#details')
   const name = f.personName.value
+  const favoriteColor = f.favoriteColor.value
+  const age = f.age.value
   
   // details.innerHTML = `<em>${name}</em>`
 
-  const em = document.createElement('em')
-  em.textContent = name
-  details.appendChild(em)
+  // const em = document.createElement('em')
+  // em.textContent = name
+  // details.appendChild(em)
+
+  details.innerHTML = `
+    <ul>
+      <li>Name: ${name}</li>
+      <li>Favorite Color: ${favoriteColor}</li>
+      <li>Age: ${age}</li>
+    </ul>
+  `
 }
 
 personForm.addEventListener('submit', handleSubmit)
