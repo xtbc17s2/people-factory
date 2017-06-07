0eb4debbd622df8f14dd144ea5a8bf72bd92fd49
Dave Strus <dave@getfretless.com>
2017-06-07 14:14:55 -0400

    Wrap everything in a block

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const personForm = document.querySelector('#personForm')
+{
+  const personForm = document.querySelector('#personForm')
 
-function renderColor(color) {
-  const div = document.createElement('div')
-  div.style.backgroundColor = color
-  div.style.width = '100px'
-  div.style.height = '50px'
+  function renderColor(color) {
+    const div = document.createElement('div')
+    div.style.backgroundColor = color
+    div.style.width = '100px'
+    div.style.height = '50px'
 
-  return div
-}
-
-function renderListItem(label, value) {
-  const item = document.createElement('li')
-  const dt = document.createElement('dt')
-  const dd = document.createElement('dd')
-  dt.textContent = label
-  dd.innerHTML = value
-  item.appendChild(dt)
-  item.appendChild(dd)
-  return item
-}
-
-function renderList(personData) {
-  const list = document.createElement('dl')
-
-  // ['name', 'favoriteColor', 'age']
-  Object.keys(personData).map(function(label) {
-    const item = renderListItem(label, personData[label])
-    list.appendChild(item)
-  })
-
-  return list
-}
-
-function handleSubmit(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const details = document.querySelector('#details')
-
-  const person = {
-    name: f.personName.value,
-    favoriteColor: renderColor(f.favoriteColor.value).outerHTML,
-    age: f.age.value,
+    return div
   }
-  
-  const list = renderList(person)
-  details.appendChild(list)
-}
 
-personForm.addEventListener('submit', handleSubmit)
+  function renderListItem(label, value) {
+    const item = document.createElement('li')
+    const dt = document.createElement('dt')
+    const dd = document.createElement('dd')
+    dt.textContent = label
+    dd.innerHTML = value
+    item.appendChild(dt)
+    item.appendChild(dd)
+    return item
+  }
+
+  function renderList(personData) {
+    const list = document.createElement('dl')
+
+    // ['name', 'favoriteColor', 'age']
+    Object.keys(personData).map(function(label) {
+      const item = renderListItem(label, personData[label])
+      list.appendChild(item)
+    })
+
+    return list
+  }
+
+  function handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const details = document.querySelector('#details')
+
+    const person = {
+      name: f.personName.value,
+      favoriteColor: renderColor(f.favoriteColor.value).outerHTML,
+      age: f.age.value,
+    }
+    
+    const list = renderList(person)
+    details.appendChild(list)
+  }
+
+  personForm.addEventListener('submit', handleSubmit)
+}
